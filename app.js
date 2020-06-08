bf63f1f39cb2e1d8cdf13afc62dd7dd4b5de0c25
Kainat Naz <49521321+Kainatn@users.noreply.github.com>
2020-06-08 18:52:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+
+// Chapter # 1
+// Task1
+
+// Task 3 
+// window.alert("Error Please Enter valid password");
+
+
+// Task 3
+// window.alert("WElcome to  the JS\n Happy Codding");
+
+// Task 4
+// alert("WElcome to  the JS land");
+// alert("Happy coding! \n ");
+
+//Task 5
+// console.log(alert("Hello... I can run js through web browser's console"));
+
+// Chapter #2
+// Task 1
+// var userName;
+
+// Task 2
+// var myNAme="kainat";
+
+// Task 3
+// var title = " Hello World";
+// alert(title);
+
+// Task 4
+// var stdName = 'john Doe';
+// var age = '15 years old';
+// var course = 'Certified Mobile Application Development';
+// alert(stdName);
+// alert(age);
+// alert(course);
+
+// Task 5
+// var pizza = "pizza\npizz\npiz\npi\np";
+// alert(pizza);
+
+//Task 6
+// var email = 'example@gmail.com';
+// alert("My email address is " + email);
+
+//Task 7
+// var book = "A smarter way to learn javaScript"
+// alert("I am trying to learn from the Book. " + book);
+
+// Task 8 
+// document.write('Yah! I can write HTML content through javaScript ');
+
+// Task 9
+// var symbols = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬'
+// alert(symbols);
+
+// Chapter 3
+// Task 1
+// var age = '20';
+// alert('I am'+ age + 'years old');
