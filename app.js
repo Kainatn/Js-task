54052a4ce72e59014a9670b6551424a3158c8c35
Kainat Naz <49521321+Kainatn@users.noreply.github.com>
2020-06-14 18:18:30 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -58,3 +58,158 @@
 // Task 1
 // var age = '20';
 // alert('I am'+ age + 'years old');
+
+// Task 2
+// var n = localStorage.getItem('on_load_counter');
+// if (n === null) {
+//     n = 0;
+// }
+// n++;
+// localStorage.setItem("on_load_counter", n);
+// nums = n.toString().split('');
+// alert('you have visited this site ' + nums + ' times');
+
+
+// Task 3
+// var  birthYear = 1996;
+// document.write('My birth year is ', birthYear);
+
+// Task 4
+// var visiterName='John Doe';
+// var ProductTitle = 'T-shirt';
+// var quantity = 5; 
+// document.write(visiterName.bold()+ ' ordered '+ quantity+' '+ ProductTitle.bold() + ' on Clothing store');
+
+
+
+
+// Chapter 5
+// Task 1
+// var firstNumber;
+// var secondNumber;
+// var firstNumber = +prompt("Enter first number");
+// var secondNumber = +prompt("Enter second number");
+// var result = firstNumber + secondNumber;
+// document.write('Sum of ' + firstNumber + ' and ' + secondNumber + ' is ' + result);
+
+// Task 2
+// var firstNumber;
+// var secondNumber;
+// var firstNumber = +prompt("Enter first number");
+// var secondNumber = +prompt("Enter second number");
+// var result = firstNumber - secondNumber;
+// document.write('Subtraction of ' + firstNumber + ' and ' + secondNumber + ' is ' + result);
+
+// var firstNumber;
+// var secondNumber;
+// var firstNumber = +prompt("Enter first number");
+// var secondNumber = +prompt("Enter second number");
+// var result = firstNumber * secondNumber;
+// document.write('Multiplication of ' + firstNumber + ' and ' + secondNumber + ' is ' + result);
+
+// var firstNumber;
+// var secondNumber;
+// var firstNumber = +prompt("Enter first number");
+// var secondNumber = +prompt("Enter second number");
+// var result = firstNumber / secondNumber;
+// document.write('Division of ' + firstNumber + ' and ' + secondNumber + ' is ' + result);
+
+// var firstNumber;
+// var secondNumber;
+// var firstNumber = +prompt("Enter first number");
+// var secondNumber = +prompt("Enter second number");
+// var result = firstNumber %  secondNumber;
+// document.write('Module of ' + firstNumber + ' and ' + secondNumber + ' is ' + result);
+
+// Task 3
+// var expression;
+// document.write('initial value is :', expression ,'<br>');
+// var numb = 5;
+// document.write('initial value is : ', numb,'<br>');
+// numb++;
+// document.write('value after increment is : ', numb,'<br>');
+// newNumb = numb + 7;
+// document.write('value after addition is : ', newNumb,'<br>');
+// newNumb--
+// document.write('value after decrement is : ', newNumb,'<br>');
+// document.write('value after increment is : ', newNumb % 2);
+
+// Task 4
+// var costOfMovie = 600;
+// var ticket = 5;
+// price = costOfMovie * ticket;
+// document.write('Total Cost to buy ', ticket, ' to a movie is ', price , 'PKR');
+
+//Task 5 
+// var number = 4;
+// document.write('Table of ', number, '<br>');
+// for (var i = 1; i <= 10; i++) {
+//     const element = i * number;
+//     document.write(number, ' x ', i, ' = ', element, "<br>");
+// }
+
+//Task 6
+// var farnhiet = 70;
+// var celsius = 25;
+// var celsiusTempreture = (farnhiet - 32) * 5 / 9;
+// var farnhietTempreture = (celsius * 9 / 5) + 32;
+// document.write(celsius, 'C is ', farnhietTempreture, "F<br>");
+// document.write(farnhiet, 'F is ', celsiusTempreture,'C');
+//Task 7
+
+// var priceItem1 = 650;
+// var priceItem2 = 100;
+// var quantityItem1 = 3;
+// var quantityItem2 = 7
+// var shippingCharges = 100;
+// var total = priceItem1 * quantityItem1 + priceItem2 * quantityItem2 + 100;
+// document.write('Total cost of your order is ', total, 'PKR');
+
+
+//Task 8
+// var totalMarks = 980;
+// var marksObtained = 804;
+// var percentage = marksObtained / totalMarks * 100;
+// document.write("Percentage: ", percentage, '%');
+
+//Task 9
+// var usDollar = 104.80;
+// var saudiRiyal = 28;
+// var currency = 10 * usDollar + 28 * 25;
+// document.write('Total Currency in PKR is : ', currency);
+
+
+//Task 10
+// var number = 2;
+// document.write('Add 5 = ', number + 2, '<br>Multiply by 10 = ', 2 * 10, '<br> Divide the result by 2 = ', number / 2);
+
+//Task 11
+// var currentYear = 2020;
+// var birthYear = 1996;
+// var yourAge = currentYear - birthYear;
+// document.write('Your age is ', yourAge);
+
+//Task 12
+// var pie = 3.142;
+// var radius = 20;
+// var circle = 2 * pie * radius;
+// var area = pie * radius * radius;
+// document.write('Radius of a circle is: ', radius, '<br>');
+// document.write('The Circumfernece is: ', circle, '<br>');
+// document.write('The area is: ', area);
+
+//Task 13
+// var favoriteSnack = 'Lays';
+// var currentAge = 15;
+// var maximumAge = 65;
+// var AmountSnackPerDay = 3;
+// var restofLife = maximumAge +currentAge   * AmountSnackPerDay;
+// document.write('You will need ', restofLife, ' Lays to last you until the ripe old Age of ', maximumAge);
+
+// Chapter 6
+// Task 1
+
+// Task 2
+// Task 3
+// Task 4
+
